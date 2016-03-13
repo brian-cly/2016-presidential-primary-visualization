ad9f879047e1dbe044bc21d87cc60c03a5e097e5
Brian Ly <briancly@berkeley.edu>
2016-03-13 00:32:34 -0800

    Implement local webserver with LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
+var webserver = require('gulp-webserver');
 
 gulp.task('coffee', function() {
 	gulp.src('coffee/script.coffee')
 	.pipe(coffee({bare: true}))
 	.pipe(gulp.dest('js'))
 });
+
+gulp.task('webserver', function() {
+	gulp.src('')
+	.pipe(webserver({
+		livereload: true,
+		open: '/index.html'
+	}))
+});
+
+gulp.task('default', ['coffee', 'webserver']);
